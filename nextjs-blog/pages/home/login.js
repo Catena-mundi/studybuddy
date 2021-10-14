22d12afc67908654c4b7c6ad5c8c44611036feb9
SofiaVega <sofia.vega18@gmail.com>
2021-10-14 10:34:26 +0900

    add simple login form and bootstrap dependencies

--- nextjs-blog/pages/home/login.js
+++ nextjs-blog/pages/home/login.js
@@ -1,15 +1,52 @@
+import React, { useState } from 'react';
 import Head from 'next/head'
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import Link from 'next/link'
-import './login.css'
+//import './login.css'
+//import "./Login.css"
 
-export default function FirstPost() {
-    return <div className = "login"><head>
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+    return <div className = "Login"><head>
       <title>Login</title>
       </head>
     <main>
     <h1 className="title">
           Welcome to study buddy
         </h1>
+    <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button block size="lg" type="submit" disabled={!validateForm()}>
+          Login
+        </Button>
+      </Form>
         
     <Link href="/">
           <a>Back to index</a>
