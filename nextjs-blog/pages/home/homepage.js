06b64d17eca775f8bd04c1d9b6097d389fdfd811
u227485 <kristina.milenkovic@sbb.ch>
2021-11-14 02:19:27 +0100

    Have the main design for the front page

--- nextjs-blog/pages/home/homepage.js
+++ nextjs-blog/pages/home/homepage.js
@@ -1,16 +1,47 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from "react-bootstrap/Container";
+import {Col, Nav, Row} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from "react-bootstrap/Card";
+
+/*
+* have to make a function where it makes all the cards corresponding to all the services that we have*/
 
 export default function FirstPost() {
     return <div><Head><title>Homepage</title></Head>
-    <main>
-    <h1 className="title">
-          Homepage
-        </h1>
-    <Link href="/">
-          <a>Back to index</a>
-    </Link>
-    </main>
+        <Navbar bg="primary" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">StudyBuddy</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="/home/homepage">Home</Nav.Link>
+                    <Nav.Link href="/home/lobby">Lobby</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+        <main>
+            <h1 className="title">Homepage</h1>
+            <Link href="/">
+                  <a>Back to index</a>
+            </Link>
+            <Row xs={1} md={2} className="g-4">
+                {Array.from({ length: 4 }).map((_, idx) => (
+                    <Col>
+                        <Card>
+                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Body>
+                                <Card.Title>Card title</Card.Title>
+                                <Card.Text>
+                                    This is a longer card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </main>
     <style jsx>{`
     main {
         padding: 5rem 0;
@@ -19,6 +50,8 @@ export default function FirstPost() {
         flex-direction: column;
         justify-content: center;
         align-items: center;
+        margin-right: 10%;
+        margin-left: 10%;
       }
     .title a {
         color: #0070f3;
