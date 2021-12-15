eb47966f6a72d3d4092835ead6d35d6a30d7ca66
SofiaVega <sofia.vega18@gmail.com>
2021-12-16 02:13:37 +0900

    do not add this changes

--- nextjs-blog/pages/components/Lobby_row.js
+++ nextjs-blog/pages/components/Lobby_row.js
@@ -1,12 +1,32 @@
+import React, { useState } from 'react';
 import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
 function Lobby_row(props) {
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     
     return <div class = "row mb-3">
         <div class = "col">{props.dataFromParent.time}</div>
         <div class = "col">{props.dataFromParent.title}</div>
         <div class = "col">{props.dataFromParent.location}</div>
         <div class = "col">
-            <Button>Join now</Button>
+            <Button onClick={handleShow}>Join now</Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Event Details</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                <div>{props.dataFromParent.time}</div>
+                <div>{props.dataFromParent.title}</div>
+                <div>{props.dataFromParent.location}</div>
+                <Button onClick={handleShow}>Add to Calendar</Button>
+                </Modal.Body>
+            </Modal>
         </div>
         <div class = "col">...</div>
     </div>;
